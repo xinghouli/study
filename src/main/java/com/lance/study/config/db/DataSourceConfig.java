093f1feb6399167fb4b7eb198b052b2542872861
credanlance <lance.li@credan.com>
2019-06-12 12:23:20 +0800

    config

--- src/main/java/com/lance/study/config/db/DataSourceConfig.java
+++ src/main/java/com/lance/study/config/db/DataSourceConfig.java
@@ -5,10 +5,14 @@ import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -32,11 +36,26 @@ public class DataSourceConfig {
         return dataSource;
     }
 
-    public SqlSessionFactory sqlSessionFactory(){
-//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        JdbcTransactionFactory jdbcTransactionFactory = new JdbcTransactionFactory();
-        Environment development = new Environment("development", jdbcTransactionFactory, dataSource());
-        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration(development);
-        return null;
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+//        String mapperResources = "classpath:mappers/**/*.xml";
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+//        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperResources));
+        bean.setDataSource(dataSource);
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(Boolean.TRUE);
+        configuration.setUseColumnLabel(Boolean.TRUE);
+        bean.setConfiguration(configuration);
+        return bean.getObject();
+    }
+
+    @Bean(name = "transactionManager")
+    public DataSourceTransactionManager dataSourceTransactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "sqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
